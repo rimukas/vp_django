d75f82fd839c8eb668d89778734cf3b14df4ea8a
rimukas <rimas.kumetaitis@gmail.com>
2016-09-21 14:49:51 +0300

    Prideti filtrai Vartotojai ir Planas lentelems admin puslapyje

--- vart/admin.py
+++ vart/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import Vartotojai, Planas
 
 # Register your models here.
+
+
+class PlanasAdmin(admin.ModelAdmin):
+	# list_display = ('upper_case_name')
+	list_filter = ('organizatorius',)
+
+
+class VartotojaiAdmin(admin.ModelAdmin):
+	list_display = ('organizatorius', 'pareigos', 'adminas', 'aktyvus')
+	list_filter = ('aktyvus',)
+
+
+admin.site.register(Vartotojai, VartotojaiAdmin)
+admin.site.register(Planas, PlanasAdmin)
--- vart/models.py
+++ vart/models.py
@@ -1,3 +1,39 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Vartotojai(models.Model):
+    organizatorius = models.CharField(max_length=50, unique=True)
+    pareigos = models.CharField(max_length=200)
+    adminas = models.BooleanField(default=False)
+    aktyvus = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = "Vartotojai"
+        verbose_name_plural = "Vartotojai"
+
+    def __str__(self):
+        return self.organizatorius
+
+
+class Planas(models.Model):
+    PDP = (('paslauga', 'Paslauga'),
+        ('darbas', 'Darbas'),
+        ('preke', 'Prekė'))
+    organizatorius = models.ForeignKey(Vartotojai,
+            to_field='organizatorius',
+            on_delete=models.CASCADE)
+    kodas = models.CharField(max_length=30)
+    preke = models.CharField(max_length=100)
+    islaidos = models.DecimalField(max_digits=9, decimal_places=2)
+    paslauga_darbas_preke = models.CharField(max_length=8,
+        default='preke',
+        choices=PDP)
+
+    class Meta:
+        verbose_name = "Planas"
+        verbose_name_plural = "Planas"
+
+    def __str__(self):
+        return self.preke
