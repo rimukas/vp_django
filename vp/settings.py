9e539e9e8e32cb2a48d33fda9018ff719b71300b
rimukas <rimas.kumetaitis@gmail.com>
2016-10-17 14:36:42 +0300

    Pakeista i PostgreSQL

--- vp/settings.py
+++ vp/settings.py
@@ -79,8 +79,15 @@ TEMPLATES = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'vp',
+        'USER': 'vpadmin',
+        'PASSWORD': 'vpadmin31495',
+        'HOST': 'localhost',
+        'PORT': '',
+
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
